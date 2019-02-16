5c98ccbb1be0c92b42f7107451ed98a012dea61c
Your Name <lv.lozano@mesfix.com>
2019-02-15 22:00:19 -0500

    old metho

--- class1.js
+++ class1.js
@@ -1,28 +1,25 @@
-const animalMethods = {
-    eat (amount){ 
-        console.log(`${this.name} is eating.`)
-        this.energy += amount // amount que es ?
-    },
-    sleep (length){
-        console.log(`${this.name} is sleeping.`)
-        this.energy += length
-    },
-    play (length){
-        console.log(`${this.name} is playing.`)
-        this.energy += length
-    }
-}
-
-
 function Animal (name, energy){ 
-    let animal = Object.create(animalMethods)
-    animal.name = name 
-    animal.energy = energy
-    
-    return animal
+    this.name = name 
+    this.energy = energy
 }
 
-Animal.prototype
+Animal.prototype.eat = function (amount) {
+    console.log(`${this.name} is eating.`)
+    this.energy += amount // amount que es ?
+}
+
+Animal.prototype.sleep = function (length) {
+    console.log(`${this.name} is sleeping.`)
+    this.energy += length
+}
+
+Animal.prototype.play = function (length) {
+    console.log(`${this.name} is playing.`)
+    this.energy += length
+}
 
 const leo = Animal('Leo',7)
 const snoop = Animal('Snoop',10)
+
+const leo = new Animal('Leo', 7)
+const snoop = new Animal('snoop', 10)
